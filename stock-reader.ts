aa79b49f9f5c3f92ff00581d58ecfe2fed351edf
angel nethaniel malca <natanelmalca@gmail.com>
2019-06-01 01:11:02 +0300

    initial commit to repository

---
+++ stock-reader.ts
@@ -0,0 +1,51 @@
+import { MAX_QUOTES  } from "./consts";
+import { StockStats } from "./stock-stats";
+
+interface IQuotes {
+    [key: string]: StockStats;
+}
+export class StockReader {
+    private alphaAPI: any;
+    private quotes: IQuotes = {};
+
+    constructor(key: string, quotes: string[]= []) {
+       this.alphaAPI = require('alphavantage')({ key });
+
+       this.initializeQuotes(quotes);
+    }
+
+    /*
+     * Re-initialize stocks to watch
+     */
+    public setStocksToWatch(quotes: string[]) {
+        this.initializeQuotes(quotes);
+    }
+
+    public initiateStockWatch() {
+        if  (Object.keys(this.quotes).length > 0) {
+            const interval = setInterval( () => {
+                this.iterateStocks();
+            }, 50 * 1000);
+        } else {
+            throw Error("No Stocks were initialized");
+        }
+    }
+
+    private initializeQuotes(quotes: string[]) {
+        const maxFiveQuotes = quotes.slice(0, MAX_QUOTES);
+        maxFiveQuotes.forEach( (quote) => {
+               this.quotes[quote] = new StockStats(quote);
+        });
+    }
+
+    private iterateStocks() {
+        Object.keys(this.quotes).forEach((quote) => {
+            const quoteStockStats: StockStats = this.quotes[quote];
+            this.alphaAPI.data.intraday(quote,'compact','json','5min').then( (data:any) => {
+                const stockInterval = data["Time Series (5min)"][Object.keys(data["Time Series (5min)"])[1]];
+                quoteStockStats.recordNewStockInterval(stockInterval);
+            });
+        });
+    }
+}
+
