3a0725552eb088bc801ea8eb7dc72fe10682da29
angel nethaniel malca <natanelmalca@gmail.com>
2019-06-26 02:16:49 +0300

    more improvements

--- stock-stats.ts
+++ stock-stats.ts
@@ -1,24 +1,34 @@
+import moment from "moment-timezone";
+
 import {  VOLUME_THRESHOLD_ALARM , MINIMUM_INTERVALS_TO_CALCULATE_AVERAGE_VOLUME } from "./config";
 import { IStockIntervalData, IAlphaVantageIntervals, IStockFullIntervalData } from "./models/stock-interval-data.model";
 
 export class StockStats {
     private quote: string;
     private volumeSum: number;
-    private iteration: number;
-    private volumeIteration: number;
+    private interval: number;
+    private volumeInterval: number;
     private avg: number;
     private todayDate: string;
     private stockIntervals: IStockFullIntervalData[] = [];
 
+
+    private isInBuyMode: boolean;
+    private boughtInterval: IStockFullIntervalData = {} as IStockFullIntervalData;
+    private ratioPower: number;
+
     constructor(quote: string) {
         this.quote = quote;
         this.volumeSum = 0;
-        this.iteration = 0;
-        this.volumeIteration = 0; 
+        this.interval = 0;
+        this.volumeInterval = 0;
         this.avg = 0;
 
+        this.isInBuyMode = false;
+        this.ratioPower = 0;
+
         // this.todayDate = moment(new Date()).format("YYYY-MM-DD");
-        this.todayDate = "2019-06-24"; // for testing
+        this.todayDate = "2019-06-25"; // for testing
     }
 
     public InitializeStockData(quoteIntervals: IAlphaVantageIntervals ) {
@@ -38,37 +48,49 @@ export class StockStats {
 
         this.calculateAverageVolume(stockInterval);
 
-        this.iteration++;
+        this.interval++;
 
-        
     }
 
     private calculateAverageVolume(stockInterval: IStockFullIntervalData) {
-        if (this.iteration >= MINIMUM_INTERVALS_TO_CALCULATE_AVERAGE_VOLUME) {
+        if (this.interval >= MINIMUM_INTERVALS_TO_CALCULATE_AVERAGE_VOLUME) {
             const {volume} = stockInterval ;
             this.volumeSum += volume;
-            this.volumeIteration++;
-            this.avg = this.volumeSum / this.volumeIteration;
+            this.volumeInterval++;
+            this.avg = this.volumeSum / this.volumeInterval;
 
             if (this.didPassVolumeThreshold( volume )) {
-                console.log(this.quote + " quote passed threshold");
+
+                this.boughtInterval = {...stockInterval };
+                this.isInBuyMode = true;
+                this.ratioPower = this.getVolumeRatioPower(volume);
+                console.log(this.quote + " quote passed threshold by " + this.ratioPower * 100 + "% at " + stockInterval.time );
             }
         }
     }
 
     private didPassVolumeThreshold(volume: number): boolean {
-        return ( volume > this.avg * VOLUME_THRESHOLD_ALARM );
+        return ( volume > this.avg * VOLUME_THRESHOLD_ALARM && !this.isInBuyMode);
+    }
+
+    private getVolumeRatioPower(volume: number): number {
+        const ratio = volume / this.avg;
+        return +ratio.toFixed(2);
     }
 
     private convertAlphaVantageFormat(stockIntervalData: IAlphaVantageIntervals, key: string): IStockFullIntervalData {
+        const nasdaqTime = moment.tz(key, "America/New_York");
+        const israelTime = nasdaqTime.clone().tz("Asia/Jerusalem");
+
         const convertedStockIntervalData: IStockFullIntervalData = {
             open :  Number(Object.values(stockIntervalData)[0]) ,
             high :  Number(Object.values(stockIntervalData)[1]),
             low :  Number(Object.values(stockIntervalData)[2]),
             close :  Number(Object.values(stockIntervalData)[3]),
             volume :  Number(Object.values(stockIntervalData)[4]),
-            time: new Date(key),
+            time: new Date( israelTime.format("YYYY-MM-DD HH:mm:ss") ),
         };
+
         return convertedStockIntervalData;
     }
 }
